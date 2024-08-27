81d3f2bc9e3174aaa3aa293939077a77ca755877
Kan <ssitinan@gmail.com>
2024-08-27 20:50:56 +0700

    Write comments that will be performed in each section of code.

--- OX_Game.c
+++ OX_Game.c
@@ -1,6 +1,38 @@
 #include <stdio.h> 
 
+// Store row and column positions.
+
+// Set player => 'x' and com => 'O'.
+
+// Checks if there are any unfilled spaces on the board.
+
+// Check the board to see if anyone has won yet?
+
+// Com : Find a possible way to win.
+
+// Player : Remember the movement
+
+// Show Board
+
+// Shows instructions on how to play the game.
+
+// initialise
+
+// Winner Announcement
+
+// Check if there are any rows on the board that have the same symbols in each row.
+
+// Check if there are any columns on the board that have the same symbols in all columns.
+
+// Check if there are any diagonals on the board that have the same markings on all diagonals.
+
+// Check if the match has ended (has a winner or the board is full).
+
+// Conduct game play between players and the computer.
+
 int main()
 {
+    // Randomly select who will start first.
+    
     return 0;
 }
